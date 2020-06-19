92430ad0b70b242d8f97f32bd05577cd2b651fb9
adam <aabou9@gmail.com>
2020-06-19 10:18:18 +0100

    fix logout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user_id = current_user.email
+        @article.user_id = current_user.id
         if @article.save
             redirect_to root_path
         else
